280435ce504ba247c4acd5e33a20816383dc020b
Lu Wang <lu-wang@MacBook-Pro.local>
2019-04-02 17:16:26 -0400

    uploads3

---
+++ uploadS3.py
@@ -0,0 +1,23 @@
+import boto3
+from botocore.client import Config
+from dataclasses import dataclass
+
+
+@dataclass()
+class S3Params(object):
+    file_name: str = "/Users/lu-wang/Documents/GitHub/boto3EMR/createEMR.py"
+    bucket_name: str = "ecomdatascience-np"
+    key: str = "lu/testUpload.py"
+    kms_key: str = "arn:aws:kms:us-east-1:729964090428:key/3a67dc71-c759-48c9-803a-22ac51fee24d"
+
+
+s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
+s3.upload_file(
+    S3Params.file_name,
+    S3Params.bucket_name,
+    S3Params.key,
+    ExtraArgs={
+        "ServerSideEncryption": "aws:kms",
+        "SSEKMSKeyId": S3Params.kms_key
+    }
+)
